134fd53b621aeef6d7e60260ef5914422695f553
Masahiro Nagano <kazeburo@gmail.com>
2013-02-27 23:22:51 +0900

    initial import

---
+++ lib/CPAN/Test/Dummy/ConfigDeps.pm
@@ -0,0 +1,41 @@
+package CPAN::Test::Dummy::ConfigDeps;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+CPAN::Test::Dummy::ConfigDeps - ...
+
+=head1 SYNOPSIS
+
+  use CPAN::Test::Dummy::ConfigDeps;
+
+=head1 DESCRIPTION
+
+CPAN::Test::Dummy::ConfigDeps is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Masahiro Nagano
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,5 @@
+use strict;
+use Test::More;
+
+BEGIN { use_ok 'CPAN::Test::Dummy::ConfigDeps' }
+done_testing;
